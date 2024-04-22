e1fe6fa8a814d6dedee2fda7bc96460897ff695f
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-04-22 21:36:29 +0800

    Update loadGEO.R

--- loadGEO.R
+++ loadGEO.R
@@ -1,8 +1,15 @@
 # download data
-
-if (!requireNamespace("BiocManager", quietly=TRUE))
-    install.packages("BiocManager")
-BiocManager::install("GEOquery")
-library(GEOquery)
-GSE156309 <- getGEO("GSE156309",destdir = 'data/')
-save.image("data/images.RData")
+pak::pak("rainoffallingstar/erebor")
+GSE_ID_methyl <- c("GSE25706","GSE29047","GSE29189",
+                   "GSE40870","GSE42042","GSE44830",
+                   "GSE58477","GSE62298","GSE63409",
+                   "GSE78963","GSE79695","GSE80508",
+                   "GSE80762","GSE86409","GSE89776")
+for (methitem in GSE_ID_methyl){
+    message(glue::glue("processing {methitem}"))
+    if (!file.exists(paste0("data/",methitem,".RDS"))){
+      methydata <- erebor::MoriaClass$new(methitem,"GEO")
+      methydata <- methydata$download()
+      saveRDS(methydata,file = paste0("data/",methitem,".RDS"))
+    }
+  }
