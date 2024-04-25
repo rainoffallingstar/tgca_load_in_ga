f3cbb311f48d3afd156f3ea49793ea9c7c06d229
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-04-25 20:59:41 +0800

    Update loadGEO.R

--- loadGEO.R
+++ loadGEO.R
@@ -5,11 +5,14 @@ GSE_ID_methyl <- c("GSE25706","GSE29047","GSE29189",
                    "GSE58477","GSE62298","GSE63409",
                    "GSE78963","GSE79695","GSE80508",
                    "GSE80762","GSE86409","GSE89776")
-for (methitem in GSE_ID_methyl){
-    message(glue::glue("processing {methitem}"))
-    if (!file.exists(paste0("data/",methitem,".RDS"))){
-      methydata <- erebor::MoriaClass$new(methitem,"GEO")
+fs::dir_create("data")
+while(length(list.files("data")) + 1 <= 15) {
+  for (i in 1:length(GSE_ID_methyl)){
+    message(glue::glue("processing {GSE_ID_methyl[i]}"))
+    if (!file.exists(paste0("data/",GSE_ID_methyl[i],".RDS"))){
+      methydata <- erebor::MoriaClass$new(GSE_ID_methyl[i],"GEO")
       methydata <- methydata$download()
-      saveRDS(methydata,file = paste0("data/",methitem,".RDS"))
+      saveRDS(methydata,file = paste0("data/",GSE_ID_methyl[i],".RDS"))
     }
   }
+}
