b93c22428c4fb66737d695f843868b3b3575302c
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-04-27 15:41:06 +0800

    Update loadGEO.R

--- loadGEO.R
+++ loadGEO.R
@@ -7,7 +7,7 @@ tiny_download <- function(dir = "data",GSE_ID_expre){
     message(glue::glue("processing {GSE_ID_expre[i]} {i}/{length(GSE_ID_expre)}"))
     if (!file.exists(paste0(dir,"/",GSE_ID_expre[i],".RDS"))){
       exprdata <- erebor::MoriaClass$new(GSE_ID_expre[i],"GEO")
-      exprdata <- exprdata$download()
+      exprdata <- exprdata$download(sup = FALSE)
       saveRDS(exprdata,file = paste0(dir,"/",GSE_ID_expre[i],".RDS"))
     }
   }
