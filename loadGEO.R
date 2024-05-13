2f69ad91c1565a72749cf4999450bd6548c64400
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-05-13 19:08:43 +0800

    Update loadGEO.R

--- loadGEO.R
+++ loadGEO.R
@@ -1,15 +1,27 @@
 # download data
-pak::pak("rainoffallingstar/erebor")
-load("0427.RData")
-tiny_download <- function(dir = "data",GSE_ID_expre){
-  fs::dir_create(dir)
-  for (i in 1:length(GSE_ID_expre)){
-    message(glue::glue("processing {GSE_ID_expre[i]} {i}/{length(GSE_ID_expre)}"))
-    if (!file.exists(paste0(dir,"/",GSE_ID_expre[i],".RDS"))){
-      exprdata <- erebor::MoriaClass$new(GSE_ID_expre[i],"GEO")
-      exprdata <- exprdata$download(sup = FALSE)
-      saveRDS(exprdata,file = paste0(dir,"/",GSE_ID_expre[i],".RDS"))
-    }
+get_metadata <- function(mine){
+  fs::dir_create("temp")
+  fs::dir_create("data")
+  data <- GEOquery::getGEO(mine,destdir = 'temp')
+  for (i in 1:1){
+    metadata <- data.frame(
+      gseid = mine,
+      platform = data[[i]]@annotation,
+      title = data[[i]]@experimentData@title,
+      abstract = data[[i]]@experimentData@abstract,
+      summary =  data[[i]]@experimentData@other$summary,
+      overall_design = data[[i]]@experimentData@other$overall_design,
+      type = data[[i]]@experimentData@other$type,
+      samplenum = nrow(data[[i]]@phenoData@data)
+    )
+   saveRDS(metadata,paste0("data/",mine,".RDS"))
   }
+  fs::dir_delete("temp")
 }
-tiny_download("data",na.omit(hsct_geo))
+amlRDS2 <- readRDS("amlrds.RDS")
+for (i in 1:1400) {
+  message(glue::glue("info:: {i}/1400"))
+  get_metadata(amlRDS2[i])
+}
+
+
